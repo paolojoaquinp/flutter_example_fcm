276d17681857920b020576594a92f9c1a0224f7a
Paolo Joaquin Pinto Perez <paolojoaquinpintoperez@gmail.com>
2024-05-24 20:29:03 -0400

    init version

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fcm/firebase_options.dart';
 
-void main() => runApp(const MyApp());
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Topicos
+  await FirebaseMessaging.instance.subscribeToTopic("topic");
+
+  // FCM Token
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  debugPrint('FCM Token: $fcmToken');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
