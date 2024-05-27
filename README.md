3ebe60ec6205075f881e4cb175695b2a97a39f52
Paolo Joaquin Pinto Perez <paolojoaquinpintoperez@gmail.com>
2024-05-27 15:40:08 -0400

    first version FCM

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
 # flutter_fcm
 
-A new Flutter project.
+Este es un ejemplo para utilizar fcm con un servidor de node.js
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+## 1. Obterner packages de Flutter
+ 
+Ejecuta el comando
+```sh
+flutter pub get
+```
 
-A few resources to get you started if this is your first Flutter project:
+## 2. Configurar El proyecto del backend 
+Obtener el backend en:
+https://github.com/paolojoaquinp/back_fcm/security/secret-scanning/unblock-secret/2h3zrfX61Bd6nEylPJ6qcK3gS5b
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+y seguir instrucciones del readme.md para ejecutarlo. 
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+## Configuración de Firebase
+
+Este proyecto utiliza Firebase. Para configurarlo, sigue estos pasos:
+
+1. Crea un nuevo proyecto en la [consola de Firebase](https://console.firebase.google.com/).
+1.1 Puedes hacer la configuracion desde `flutterfire configure` si es asi saltar demas pasos de este bloque(Configuración de Firebase).
+2. Agrega una nueva aplicación Android a tu proyecto de Firebase.
+3. Descarga el archivo `google-services.json` y colócalo en la carpeta `android/app` de este proyecto.
+4. Agrega una nueva aplicación iOS a tu proyecto de Firebase.
+5. Descarga el archivo `GoogleService-Info.plist` y colócalo en la carpeta `ios/Runner` de este proyecto.
+
+Nota: No olvides habilitar los servicios que necesites en la consola de Firebase (por ejemplo, autenticación, base de datos, etc.).
+
+
+## 2.1 Ejecutar app Flutter(y minimizar para tenerlo en segundo plano)
+
+Al iniciar la app podras ver en DEBUG CONSOLE o en tu terminal la salida(output):
+FCM Token: y-varias-letras
+
+y-varias-letras es tu token necesitas copiar el token para el paso siguiente.
+
+## 3 Ejecutar endpoint desde Postman o ThunderClient 
+
+metodo: POST
+body: {
+    "fcmToken": "mi-token-generado-desde-la-consola-de-app"
+}
+
+endpoint: http://localhost:3000/sendNotification/
+
+Y YA!
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fcm/firebase_options.dart';
+
+import 'package:http/http.dart' as http;
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
@@ -12,6 +16,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
   print("Handling a background message: ${message.messageId}");
 }
+
 
 
 void main() async {
@@ -25,9 +30,7 @@ void main() async {
   // Topicos
   await FirebaseMessaging.instance.subscribeToTopic("topic");
 
-  // FCM Token
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  debugPrint('FCM Token: $fcmToken');
+
 
   runApp(const MyApp());
 }
@@ -43,14 +46,60 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: HomePage()
     );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FCM Token'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              var client = http.Client();
+              try {
+              final fcmTokenResp = await FirebaseMessaging.instance.getToken();
+              debugPrint('FCM Token: $fcmTokenResp');
+
+              // Salir de la aplicación
+              Navigator.of(context).pop();
+
+              // Esperar 3 segundos
+              await Future.delayed(Duration(seconds: 3));
+
+              var url = Uri.parse('http://192.168.1.9:3000/sendNotification');
+              final response = await client.post(
+                url,
+                // {"fcmToken": "fcmTokenResp"}
+                body: jsonEncode(<String, String>{
+                  'fcmToken': fcmTokenResp!,
+                }),
+                headers: {
+                  'Content-Type': 'application/json',
+                }
+              );
+              var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+              debugPrint('Response: ${decodedResponse.toString()}');
+              } catch (e) {
+              } finally {
+                client.close();
+              }
+            },
+            child: const Text('Obtener Token y enviar al servidor')
+          ),
+        ),
+      );
   }
 }
